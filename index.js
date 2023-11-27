8d4f296371ce0551af8e57490fac66fc94b4664b
MichaelPile <mjpile@gmail.com>
2023-11-27 15:25:33 -0800

    completes objects lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+const employee = {
+    name : "Michael",
+    streetAddress : "149 Parkway",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+
+        return {
+            ...employee, [key] : value
+        };
+    }
+
+//console.log(updateEmployeeWithKeyAndValue(employees, "Michael", "14 Driveway") );
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmpl = {...employee};
+    delete newEmpl[key];
+    return newEmpl;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
+//console.log(deleteFromEmployeeByKey(employee, 'name'));
